21b10a3437ed8cfad4b93ebbd910056f48ea8f19
Mauricio <mauriciorojascareers@gmail.com>
2024-08-25 23:49:49 -0500

    clickables

---
+++ src/app/book/page.tsx
@@ -0,0 +1,88 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+import { addDays, format } from "date-fns";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import React from "react";
+
+import { Calendar as CalendarIcon, Loader2 } from "lucide-react";
+
+import { cn } from "@/lib/utils";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+export default function Book() {
+  const [date, setDate] = React.useState<Date>();
+  const [open, setOpen] = React.useState(false);
+
+  const handleDateSelect = (selectedDate: Date | undefined) => {
+    setDate(selectedDate);
+    setOpen(false); // Close the Popover when a date is selected
+  };
+
+  return (
+    <div className="flex flex-col items-center p-4 h-screen">
+      {/* <div className="flex text-center"> */}
+      <h2 className="text-slate-900 text-xl ">Pick a date:</h2>
+      {/* </div> */}
+      <div className=" flex flex-col md:flex-row md:justify-around md:items-center gap-4 p-4 ">
+        <div className="">
+          <Popover open={open} onOpenChange={setOpen}>
+            <PopoverTrigger asChild>
+              <Button
+                variant={"outline"}
+                className={cn(
+                  "w-[280px] justify-start text-left font-normal",
+                  !date && "text-muted-foreground"
+                )}
+              >
+                <CalendarIcon className="mr-2 h-4 w-4" />
+                {date ? format(date, "PPP") : <span>Pick a date</span>}
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent className="flex w-auto flex-col space-y-2 p-2">
+              <Select
+                onValueChange={(value) =>
+                  handleDateSelect(addDays(new Date(), parseInt(value)))
+                }
+              >
+                <SelectTrigger>
+                  <SelectValue placeholder="Select" />
+                </SelectTrigger>
+                <SelectContent position="popper">
+                  <SelectItem value="0">Today</SelectItem>
+                  <SelectItem value="1">Tomorrow</SelectItem>
+                  <SelectItem value="3">In 3 days</SelectItem>
+                  <SelectItem value="7">In a week</SelectItem>
+                </SelectContent>
+              </Select>
+              <div className="rounded-md border">
+                <Calendar
+                  mode="single"
+                  selected={date}
+                  onSelect={handleDateSelect}
+                />
+              </div>
+            </PopoverContent>
+          </Popover>
+        </div>
+        <div className="  flex justify-center mt-60 md:mt-0">
+          <Button disabled>
+            <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+            Book Now
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
